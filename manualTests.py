e41ab0d44f9964811764649a09325dfe078df2fa
Javier Refuerzo <javierrefuerzo@gmail.com>
2022-01-24 17:51:43 -0700

    removed nls and devices node def

--- manualTests.py
+++ manualTests.py
@@ -10,6 +10,8 @@ Copyright (C) 2021 Javier Refuerzo
 from typing import List
 from iTachLib.controller.irCode import IrCode
 from iTachLib.controller.controller import Controller as ITACH
+from iTachLib.controller.Device import Device
+from objects.DocumentModifier import DocumentModifier
 
 
 #codeSet = CodeSet()
@@ -29,29 +31,29 @@ irCode1 = IrCode("POWER TOGGLE", "sendir,0:1,1,38000,1,69,339,170,21,21,21,63,21
 irCode2 = IrCode("Test Button2", "sendir,1:1,1,38000,1,69,339,170,21,21,21,63,21,21,21,21,21,63,21,21,21,63,21,63,21,63,21,21,21,63,21,63,21,21,21,63,21,63,21,21,21,63,21,21,21,63,21,63,21,63,21,21,21,63,21,21,21,21,21,63,21,21,21,21,21,21,21,63,21,21,21,63,21,1466,339,84,21,3633")
 
 command = irCode1.command(buttonCode=1, connector=1, repeat=1)
-iTach = ITACH(address="http://192.168.0.70", observers=None)
+iTach = ITACH(address="http://192.168.1.70", observers=None)
 response = iTach.send_command(command=command)
 
 
 
-if response != None:
-    print("Command: " + response)
-else:
-    print("No response" )
+# if response != None:
+#     print("Command: " + response)
+# else:
+#     print("No response" )
 
-# irList: List[IrCode] = []
-# irList.append(irCode1)
-# irList.append(irCode2)
+irList: List[IrCode] = []
+irList.append(irCode1)
+irList.append(irCode2)
 
-# device1 = Device("device One", irList)
-# device2 = Device("device Two", irList)
+device1 = Device("device One", irList)
+device2 = Device("device Two", irList)
 
-# deviceList: List[Device] = []
-# deviceList.append(device1)
-# deviceList.append(device2)
+deviceList: List[Device] = []
+deviceList.append(device1)
+deviceList.append(device2)
 
-# mod = DocumentModifier()
-# mod.writeFiles(deviceList)
+mod = DocumentModifier()
+mod.writeFiles(deviceList)
 
 
 print("Finished")
